2ab4ae9c0122875ff199a329f8395af55f1a5e51
yingliu <yingliu4203@gmail.com>
2020-04-09 16:07:49 -0700

    add home page

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+const multer = require('multer')
+const upload = multer()
+
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+
+app.get('/', (request, response) => {
+  const helloFile = __dirname + '/views/index.html'
+  response.sendFile(helloFile)
+})
+
+const listener = app.listen(3000, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`)
+})
