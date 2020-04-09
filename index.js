95226a7894f041091d1af221db088661dbcb2170
yingliu <yingliu4203@gmail.com>
2020-04-09 16:28:52 -0700

    add post data

--- index.js
+++ index.js
@@ -15,3 +15,18 @@ app.get('/', (request, response) => {
 const listener = app.listen(3000, () => {
   console.log(`Your app is listening on port ${listener.address().port}`)
 })
+
+// Serve content of the "public" subfolder directly
+app.use(express.static('public'))
+
+app.post('/animals', upload.array(), (request, response) => {
+  const name = request.body.name
+  const vote = request.body.strongest
+  response.send(`Hello ${name}, you voted: ${vote}`)
+})
+
+// Handle submission of a JSON car array
+app.post('/api/cars', jsonParser, (request, response) => {
+  const cars = request.body
+  response.send(`You sent me a list of cars: ${JSON.stringify(cars)}`)
+})
